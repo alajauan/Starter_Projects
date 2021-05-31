15fb94a69637575b77fa5633ae21abc389845236
alajauan <84892350+alajauan@users.noreply.github.com>
2021-05-31 08:57:01 -0400

    Add files via upload

---
+++ what_we_drinking.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+
+
+
+print ("Welcome to What We Drinking")
+
+import sys
+
+age= int(input ("Please Enter Your Age: ")) #only those over 21 should enter
+ 
+for i in range (0, 1): 
+   
+         if age >= 21 :
+            print("Enter 1 for Rum\n")
+            print("Enter 2 for Vodka\n")         
+            print("Enter 3 for Tequila\n")
+
+         if age < 21:
+            print ("We are sorry you do not meet the age requirement")
+            sys.exit()
+ 
+
+import random
+
+rum =["Rum Punch", "Mojito", "Mai Tai", "Dark ’n Stormy", "Piña Colada", "Hurricane" ]
+#rum drinks
+rum_type = rum[random.randint(0, len(rum) -1)]
+
+vodka =["Screwdriver", "Vodka Martini", "Cosmopolitan", "Vodka Tonic", "Moscow Mule", "Bloody Mary" ]
+#vodka drinks
+vodka_type = vodka[random.randint(0, len(vodka) -1)]
+
+tequlia= ["Margarita", "Tequila Sunrise", "Bloody Maria", "El Diablo", "Sangrita", " Paloma"]
+#tequlia Drinks
+tequlia_type = tequlia[random.randint(0, len(tequlia) -1)]
+
+choice = int(input("Enter your choice:"))
+    
+for i in range (0,1):
+  
+   if choice == 1 :
+       print (rum_type)
+   elif choice == 2 :
+       print (vodka_type)
+   elif choice == 3 :
+        print (tequlia_type)
+   else:
+        
+        print ("Invalid Selection")
+   
+
+
+
+print ("Drink Up & Enjoy")
+
+
+
+
